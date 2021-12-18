33c73f80fb39f0da94bdadc1290c97863176e2f4
AdemirRamos <ademirramos634@gmail.com>
2021-12-18 17:31:29 -0300

    Adição da Aula 2

---
+++ CFB Cursos/Aula 2/aula2.py
@@ -0,0 +1,21 @@
+#Em Python, as variáveis são dinâmicas. Dependendo do valor que recebem, o seu tipo mudará.
+
+num = 1 #Número inteiro (int);
+num = 'Bruno' #"String";
+num = 1.5 #Número real (float).
+
+#Em outras linguagens, o "terminador de comando" é marcado por ";" (ponto e vírgula).
+#Em Python, uma quebra de linha representa o "terminador de comando".
+#Por outro lado, é possível usar ";" (ponot e vírgula) em Python.
+#Caso se deseje ter mais de um comando em uma mesma linha, é possível usar o ";" (ponto e vírgula):
+
+canal = 'CFB Cursos'; curso = 'Curso de Python'; print(f'Canal: {canal}; Curso: {curso}.')
+a = b = c = d = 0
+
+#Se pode definir o espaço ("parágrafo") de escopo para os comandos como se desejar. Pode ser um ou mais esapços ou um ou mais "tabs".
+
+if 10 > 2:
+   print('Maior')
+else:
+    print('Menor')
+print('Fim')
