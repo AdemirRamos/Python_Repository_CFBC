1989d98ab372f7938854ab675f2b2d24b8e64387
AdemirRamos <ademirramos634@gmail.com>
2021-12-19 04:58:56 -0300

    Atualização da Aula 8

--- CFB Cursos/Aula 8/aula8.py
+++ CFB Cursos/Aula 8/aula8.py
@@ -8,3 +8,16 @@ b = False
 c = 10 > 15
 
 print(f'{a}, {b}, {c}.')
+
+#Usando "casting" para converter uma "string" para "boolean":
+
+aula = 'Python'
+aula_2 =''
+
+print(f'\nValor da variável "aula": {bool(aula)}.')
+print(f'\nValor da variável "aula_2": {bool(aula_2)}.')
+
+if aula: #Significa: se a aula tem como valor "True".
+    print('Possui texto.')
+else:
+    print('Não possui texto.')
