440c847e7972efaec28d0886a511273cad8ead52
AdemirRamos <ademirramos634@gmail.com>
2021-12-19 04:54:43 -0300

    Atualização da Aula 8

--- CFB Cursos/Aula 8/aula8.py
+++ CFB Cursos/Aula 8/aula8.py
@@ -2,3 +2,9 @@ print('Tipo Boolean')
 print()
 
 #Basicamente, o tipo "Boolean" representa se um conteúdo é verdaderiro ("True") ou falso ("False").
+
+a = True
+b = False
+c = 10 > 15
+
+print(f'{a}, {b}, {c}.')
