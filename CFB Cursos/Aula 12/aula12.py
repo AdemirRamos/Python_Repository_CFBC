097b7916fa7bedcec50aa9568e2eea1e3fe1111b
AdemirRamos <ademirramos634@gmail.com>
2021-12-20 13:16:39 -0300

    Atualização da Aula 12

--- CFB Cursos/Aula 12/aula12.py
+++ CFB Cursos/Aula 12/aula12.py
@@ -7,3 +7,5 @@ print(carros)
 for x in carros:
     print(f'Carro: {x}')
 
+#A variável "x" se torna a chave dos elementos da Lista, ou seja, "x" representa cada elemento da Lista.
+#Nesse caso, o "loop" só termina quando todos os elementos forem impressos.
