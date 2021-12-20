e68ca7ddc2f741ff1e59516970e5211ec9b7a669
AdemirRamos <ademirramos634@gmail.com>
2021-12-20 13:27:33 -0300

    Atualização da Aula 12

--- CFB Cursos/Aula 12/aula12.py
+++ CFB Cursos/Aula 12/aula12.py
@@ -4,10 +4,11 @@ print()
 carros = ['HRV', 'Golf', 'Argo', 'Focus']
 print(carros)
 
+print()
 for x in carros:
-    print(f'\nCarro: {x}')
-    if x == 'Golf': #É possível colocar o teste lógico entre parênteses: "if (x == 'Golf'):".
-        print(' VW')
+    print(f'Carro: {x}')
+    if x == 'Golf' : #É possível colocar o teste lógico entre parênteses: "if (x == 'Golf'):".
+        print(f'({carros[1]} VW)')
 
 
 #A variável "x" se torna a chave dos elementos da Lista carros, ou seja, "x" representa cada elemento da Lista.
