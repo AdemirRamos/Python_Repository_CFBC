37fbd42ae37868392377aec0320249a94a81cff9
AdemirRamos <ademirramos634@gmail.com>
2021-12-20 13:19:23 -0300

    Atualização da Aula 12

--- CFB Cursos/Aula 12/aula12.py
+++ CFB Cursos/Aula 12/aula12.py
@@ -5,7 +5,11 @@ carros = ['HRV', 'Golf', 'Argo', 'Focus']
 print(carros)
 
 for x in carros:
-    print(f'Carro: {x}')
+    print(f'\nCarro: {x}')
+    if x == 'Golf': #É possível colocar o teste lógico entre parênteses: "if (x == 'Golf'):".
+        print(' VW')
 
-#A variável "x" se torna a chave dos elementos da Lista, ou seja, "x" representa cada elemento da Lista.
+
+#A variável "x" se torna a chave dos elementos da Lista carros, ou seja, "x" representa cada elemento da Lista.
 #Nesse caso, o "loop" só termina quando todos os elementos forem impressos.
+
