fcee6d201145db62ac2c2a3c5ad3c20f24750f58
AdemirRamos <ademirramos634@gmail.com>
2021-12-21 01:00:12 -0300

    Atualização da Aula 14

--- CFB Cursos/Aula 14/aula14.py
+++ CFB Cursos/Aula 14/aula14.py
@@ -5,6 +5,9 @@ import os
 os.system('cls')
 
 #Regras do While:
+
 #1ª: inicialização de variável de controle;
 #2ª: condição / teste lógico;
 #3ª: enquanto o teste lógico retornar "True", o programa continuará; quando o resultado for "False", o programa parará.
+
+#Caso a variável de controle sempre retorne "True", será iniciado um "loop infinito" (o programa não parará sozinho).
