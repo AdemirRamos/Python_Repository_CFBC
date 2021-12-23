2acd04139471f9129ff1d3da4d91523aef7493f5
AdemirRamos <ademirramos634@gmail.com>
2021-12-23 09:21:40 -0300

    Atualização da Aula 20

--- CFB Cursos/Aula 20/aula20.py
+++ CFB Cursos/Aula 20/aula20.py
@@ -15,3 +15,12 @@ def textos(*txt): #A esse argumento (precedido por um asterisco), se dá o nome 
         print(f'O texto digitado foi: "{t}".')
 
 textos('Olá, mundo!', 'Meu nome é Ademir', 'Tudo bem com vocês?')
+
+def somar_2(* num):
+    print()
+    r = 0
+    for n in num:
+        r += n
+    print(f'O resultado da soma dos números apresentados é igual a: {r}.')
+
+somar_2(5, 7, 9, 3)
