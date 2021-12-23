a6e687f8c0042159d704f7b6b38fdbb11b5dca56
AdemirRamos <ademirramos634@gmail.com>
2021-12-23 09:18:01 -0300

    Atualização da Aula 20

--- CFB Cursos/Aula 20/aula20.py
+++ CFB Cursos/Aula 20/aula20.py
@@ -8,3 +8,10 @@ def somar(n1, n2): #A essas variáveis, é dado o nome "parâmetro de entrada".
     print(f'A soma dos valores apresentados é igual a: {soma}.')
 
 somar(7, 9)
+
+def textos(*t): #A esse argumento (precedido por um asterisco), se dá o nome de "argumento arbitrário".
+    print()
+    for p in t:
+        print(f'O texto digitado foi: "{p}".')
+
+textos('Olá, mundo!', 'Meu nome é Ademir', 'Tudo bem com vocês?')
