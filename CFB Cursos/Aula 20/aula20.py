d46aac6308b9bb4f64c43f52d5d34e1bd6bb3502
AdemirRamos <ademirramos634@gmail.com>
2021-12-23 09:13:49 -0300

    Atualização da Aula 20

--- CFB Cursos/Aula 20/aula20.py
+++ CFB Cursos/Aula 20/aula20.py
@@ -5,6 +5,6 @@ import os; os.system('cls')
 
 def somar(n1, n2): #A essas variáveis, é dado o nome "parâmetro de entrada".
     soma = n1 + n2
-    print(soma)
+    print(f'A soma dos valores apresentados é igual a: {soma}.')
 
 somar(7, 9)
