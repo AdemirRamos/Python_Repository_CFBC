e9cb7149b102fee239e9247b73325eb76055866a
AdemirRamos <ademirramos634@gmail.com>
2021-12-25 10:29:45 -0300

    Atualização da Aula 23

--- CFB Cursos/Aula 23/aula23.py
+++ CFB Cursos/Aula 23/aula23.py
@@ -3,3 +3,6 @@ print()
 
 import os; os.system('cls')
 
+#O que é uma Classe? De forma resumida:
+#É a especificação de um Objeto; um conjunto de regras para um Objeto.
+#O Objeto, por sua vez, é uma instância da Classe.
