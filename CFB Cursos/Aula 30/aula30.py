e76fc7e3401b49bf12ccfa0863d5605030c8f40f
AdemirRamos <ademirramos634@gmail.com>
2022-01-06 15:35:21 -0300

    Atualização da Aula 30

--- CFB Cursos/Aula 30/aula30.py
+++ CFB Cursos/Aula 30/aula30.py
@@ -17,13 +17,13 @@ def tela():
     global velha
     global jogadas
     os.system('cls')
-    print('    0   1   2')
-    print('0:  ' + velha[0][0] + ' | ' + velha[0][1] + ' | ' + velha[0][2])
+    print('    0   1   2\n')
+    print(f'0:  {velha[0][0]} | {velha[1][0]} | {velha[2][0]}')
     print('   -----------')
-    print('0:  ' + velha[1][0] + ' | ' + velha[1][1] + ' | ' + velha[1][2])
+    print(f'1:  {velha[1][0]} | {velha[1][0]} | {velha[2][0]}')
     print('   -----------')
-    print('0:  ' + velha[2][0] + ' | ' + velha[2][1] + ' | ' + velha[2][2])
-    print('\nJogadas fetas' + Fore.GREEN + str(jogadas) + Fore.RESET)
+    print(f'2:  {velha[0][0]} | {velha[1][0]} | {velha[2][0]}')
+    print(f'\nJogadas feitas: {Fore.GREEN} {jogadas} {Fore.RESET}.')
 
-while True:
-    tela()
+#while True:
+tela()
