91009fedd4b7a64b80f9bed601db093b48e28ec4
AdemirRamos <ademirramos634@gmail.com>
2022-01-08 16:34:14 -0300

    Atualização da Aula 38

--- CFB Cursos/Aula 38/aula38.py
+++ CFB Cursos/Aula 38/aula38.py
@@ -5,47 +5,47 @@ print()
 
 import json
 
-json_37 = '{"nome": "Bruno", "time": "aviadores","vivo": "True", "energia": "100", "mochila": ["pederneira, "corda", "linha", "arame"], "aeronaves:" [{"tipo": "transporte", "habilidade": "80"}, {"tipo": "ataque", "habilidade": "100"}, {"tipo": "reconhecimento", "habilidade": "50"}]}'
+json_37 = '{"nome": "Bruno", "time": "aviadores","vivo": "True", "energia": "100", "mochila": ["pederneira", "corda", "linha", "arame"], "aeronaves": [{"tipo": "transporte", "habilidade": "80"}, {"tipo": "ataque", "habilidade": "100"}, {"tipo": "reconhecimento", "habilidade": "50"}]}'
 
 itens = json.loads(json_37)
 
 #Impressão das chaves:
 for c in itens:
-    print(c)
+    print(f'Chave: {c}')
 
 #Impressão dos itens:
 print()
 for i in itens.items():
-    print(i)
+    print(f'Item: {i}')
 
 #Impressão dos valores:
 print()
 for v in itens.values():
-    print(v)
+    print(f'Valor: {v}')
 
 #Nome do Jogador:
 print()
-print(itens['nome'])
+print(f'Nome do jogador: {itens["nome"]}')
 
 #Itens da mochila:
 print()
 for itens_mochila in itens['mochila']:
-    print(itens_mochila)
+    print(f'Item da mochila: {itens_mochila}')
 
 #Aeronaves:
 print()
 for a in itens['aeronaves']:
-    print(a)
+    print(f'Item de "aeronaves": {a}')
 
 #Tipos:
 print()
 for t in itens['aeronaves']:
-    print(t['tipo'])
+    print(f'Tipo de "aeronaves": {t["tipo"]}')
 
 #Habilidades:
 print()
 for h in itens['aeronaves']:
-    print(h['habilidade'])
+    print(f'Habilidade de "aeronaves": {h["habilidade"]}')
 
 #Tipos e habilidades:
 print()
