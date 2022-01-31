434c09bec995114c523f20e7c6641e82a206e87c
AdemirRamos <ademirramos634@gmail.com>
2022-01-31 14:46:20 -0300

    Atualização da Aula 76

--- CFB Cursos/Aula 76/aula76.py
+++ CFB Cursos/Aula 76/aula76.py
@@ -1,15 +1,39 @@
 import os; os.system('cls')
 
-print('Tkinter - Abas e "Notbook"')
+print('Tkinter - Abas e "Notebook"')
 print()
 
-from tkinter import *
+from tkinter import *; from tkinter import ttk
 
 janela = Tk()
 
-janela.title='CFB Cursos'
+janela.title=('CFB Cursos')
 janela.geometry('500x300')
 
+#Por este se tratar do primeiro elemento, o seu pai será a "janela".
 
+notebook = ttk.Notebook(janela)
+notebook.place(x=0, y=0, width=500, height=300)
+
+#Adicionandos os "frames" (que serão as abas de "notebook"):
+
+aba_1 = Frame(notebook)
+aba_2 = Frame(notebook)
+
+#Adicionando os "frames" ao "notebook":
+
+notebook.add(aba_1, text='Cursos')
+notebook.add(aba_2, text='Canal')
+
+#Adicionando conteúdo às abas:
+
+label_1 = Label(aba_1, text='Curso de Python')
+label_1.pack()
+
+label_2 = Label(aba_1, text='Curso de ReactNative')
+label_2.pack()
+
+label_3 = Label(aba_2, text='youtube.com/cfbcursos')
+label_3.pack()
 
 janela.mainloop()
