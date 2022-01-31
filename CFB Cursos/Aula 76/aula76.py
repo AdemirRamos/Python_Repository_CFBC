38da929168eace6d3d06dd9cb0d4e14a216d7ae1
AdemirRamos <ademirramos634@gmail.com>
2022-01-30 23:36:47 -0300

    Adição da Aula 76

---
+++ CFB Cursos/Aula 76/aula76.py
@@ -0,0 +1,15 @@
+import os; os.system('cls')
+
+print('Tkinter - Abas e "Notbook"')
+print()
+
+from tkinter import *
+
+janela = Tk()
+
+janela.title='CFB Cursos'
+janela.geometry('500x300')
+
+
+
+janela.mainloop()
