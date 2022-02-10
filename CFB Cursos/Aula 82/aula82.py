ae893ed6d8d78956841391d53e10b5301b977731
AdemirRamos <ademirramos634@gmail.com>
2022-02-10 10:54:26 -0300

    Adição da Aula 82

---
+++ CFB Cursos/Aula 82/aula82.py
@@ -0,0 +1,15 @@
+import os; os.system('cls')
+
+print('Tkinter - TreeView Com Banco de Dados')
+print()
+
+from tkinter import *
+
+janela = Tk()
+
+janela.title('CFB Cursos')
+janela.geometry('500x300')
+
+
+
+janela.mainloop()
