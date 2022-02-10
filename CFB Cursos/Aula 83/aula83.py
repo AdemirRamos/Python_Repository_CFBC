efbc1a3abb8bb879d1c72fc22a65be847f7089f3
AdemirRamos <ademirramos634@gmail.com>
2022-02-10 11:02:06 -0300

    Adição da Aula 83

---
+++ CFB Cursos/Aula 83/aula83.py
@@ -0,0 +1,15 @@
+import os; os.system('cls')
+
+print('Criando Arquivos PDF, Em Python, Com Reportlab (Parte 1)')
+print()
+
+from tkinter import *
+
+janela = Tk()
+
+janela.title('CFB Cursos')
+janela.geometry('500x300')
+
+
+
+janela.mainloop()
