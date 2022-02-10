46c7d5716a5fab0834928052a8e2fd034781e7d3
AdemirRamos <ademirramos634@gmail.com>
2022-02-10 10:27:52 -0300

    Atualização da Aula 80

--- CFB Cursos/Aula 80/aula80.py
+++ CFB Cursos/Aula 80/aula80.py
@@ -4,10 +4,17 @@ from turtle import width; os.system('cls')
 print('Tkinter - Inserindo Dados do Formulário no TreeView')
 print()
 
-from tkinter import *; from tkinter import ttk
+from tkinter import *; from tkinter import ttk; from tkinter import messagebox
 
 def inserir():
-    print()
+    if variável_id.get() == '' or variável_nome.get() == '' or variável_telefone.get() == '':
+        messagebox.showinfo(title='ERRO', message='Um ou mais campos estão vazios.')
+        return #O "return" irá parar a execução desta função.
+    
+    tree_view.insert('', 'end', values=(variável_id.get(), variável_nome.get(), variável_telefone.get()))
+    
+    variável_id.delete(0, END); variável_nome.delete(0, END); variável_telefone.delete(0, END)
+    variável_id.focus()
 
 def deletar():
     print()
@@ -27,7 +34,7 @@ label_nome = Label(janela, text='Nome') #anchor=W
 variável_nome = Entry(janela)
 
 label_telefone = Label(janela, text='Telefone') #anchor=W
-variável_id = Entry(janela)
+variável_telefone = Entry(janela)
 
 tree_view = ttk.Treeview(janela, columns=('id', 'nome', 'telefone'), show='headings')
 
@@ -50,7 +57,7 @@ label_nome.grid(column=1, row=0, stick='w')
 variável_nome.grid(column=1, row=1)
 
 label_telefone.grid(column=2, row=0, stick='w')
-variável_id.grid(column=2, row=1, stick='w')
+variável_telefone.grid(column=2, row=1, stick='w')
 
 tree_view.grid(column=0, row=3, columnspan=3, pady=5)
 
